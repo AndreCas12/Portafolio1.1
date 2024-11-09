3ba4a57c7656b9fcc1087be5a0cb3c16a7463435
AndreCas12 <135380632+AndreCas12@users.noreply.github.com>
2024-11-09 12:54:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+window.onload = function () {
+    setTimeout(function () {
+        window.location.href = "principal.html";  // Redirige a otra página (ajusta la URL si es necesario)
+    }, 3000);  // 3 segundos de carga (ajusta el tiempo si lo necesitas)
+};
